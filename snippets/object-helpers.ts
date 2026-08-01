f9f0d3096c973570ce741f9484ca4564ba3c3caf
rizni <178656223+rizinthehub@users.noreply.github.com>
2026-08-01 22:07:23 +0530

    feat: add object helper utilities

---
+++ snippets/object-helpers.ts
@@ -0,0 +1,47 @@
+/**
+ * Object Utility Functions
+ */
+
+/**
+ * Pick specific keys from an object
+ */
+export function pick<T extends object, K extends keyof T>(
+  obj: T,
+  keys: K[]
+): Pick<T, K> {
+  const result = {} as Pick<T, K>;
+  keys.forEach((key) => {
+    if (key in obj) {
+      result[key] = obj[key];
+    }
+  });
+  return result;
+}
+
+/**
+ * Omit specific keys from an object
+ */
+export function omit<T extends object, K extends keyof T>(
+  obj: T,
+  keys: K[]
+): Omit<T, K> {
+  const result = { ...obj };
+  keys.forEach((key) => {
+    delete result[key];
+  });
+  return result as Omit<T, K>;
+}
+
+/**
+ * Deep clone an object
+ */
+export function deepClone<T>(obj: T): T {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+/**
+ * Check if object is empty
+ */
+export function isEmpty(obj: object): boolean {
+  return Object.keys(obj).length === 0;
+}
